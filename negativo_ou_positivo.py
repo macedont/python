8bf3d28c508f69a29948c268f53b704170bf6518
macedont <51675776+macedont@users.noreply.github.com>
2019-06-20 23:30:10 -0300

    Create negativo_ou_positivo.py

---
+++ negativo_ou_positivo.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+= float(input("Informe um numero: "))
+
+if n > 0
+    print("{} é positivo".format(n))
+else:
+    print("{} é negativo".format(n))
