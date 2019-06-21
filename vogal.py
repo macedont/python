4751489397fa548f3e99ff44bc2008b2fb195071
macedont <51675776+macedont@users.noreply.github.com>
2019-06-20 23:28:50 -0300

    Update vogal.py

--- vogal.py
+++ vogal.py
@@ -1,3 +1,5 @@
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
 letra = str(input("Informe uma letra para saber se é vogal ou consoante: ")).lower()
 
 if letra in ["a", "e", "i", "o", "u"]:
